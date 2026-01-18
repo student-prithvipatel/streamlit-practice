2637225e320a6a54c98702789d53825c994e562b
Prithvi Patel <prithvitpatel@gmail.com>
2026-01-18 13:24:09 +0530

    Add age input, rating slider, and button functionality

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ st.subheader("Subheader")
 st.text("Simple text")
 st.write("Any data")
 st.markdown("**Bold Text**")
+
 age = st.number_input("Enter age", 0, 100)
 st.write(age)
+rating= st.slider("rate",0,10)
+st.write(rating)
+
+if st.button("Click Me"):
+    st.write("Button Clicked")
